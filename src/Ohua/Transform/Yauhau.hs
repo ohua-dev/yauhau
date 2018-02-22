58af9777f3ee78a7e8e4cfcc1368008d819b9320
Justus Adam <dev@justus.science>
2018-02-22 13:17:46 +0100

    Commiting graph manipulation code
    This will be removed again but perhaps we can reuse some of this later
    so I'm saving it

--- src/Ohua/Transform/Yauhau.hs
+++ src/Ohua/Transform/Yauhau.hs
@@ -1,14 +1,20 @@
 module Ohua.Transform.Yauhau where
 
 
+import           Control.Arrow
 import           Control.Comonad.Env               (env, runEnv)
 import           Control.Monad.Reader              as R
+import           Control.Monad.State
 import           Control.Monad.Writer
 import           Data.Foldable
 import           Data.Functor.Foldable
-import           Data.Generics.Uniplate.Operations hiding (para)
+import           Data.Generics.Uniplate.Operations hiding (contexts, para)
+import           Data.Graph.Inductive.Graph
+import           Data.Graph.Inductive.PatriciaTree
 import qualified Data.HashMap.Strict               as HM
 import qualified Data.HashSet                      as HS
+import qualified Data.IntSet                       as IntSet
+import           Data.Maybe
 import           Ohua.ALang.Lang
 import           Ohua.ALang.Passes                 (normalize)
 import qualified Ohua.ALang.Refs                   as Refs
@@ -144,8 +150,12 @@ findFetchId e = case [ i | Var (Sf ty i) <- universe e, ty == Refs.smap ] of
                   _         -> Nothing
 
 
+fetchName :: QualifiedBinding
+fetchName = "yauhau/fetch"
+
+
 fetchSF, zipSF, unzipSF, smapSF :: Expression
-fetchSF = Var $ Sf "yauhau/fetch" Nothing
+fetchSF = Var $ Sf fetchName Nothing
 zipSF = Var $ Sf "yauhau/zip" Nothing
 unzipSF = Var $ Sf "yauhau/unzip" Nothing
 smapSF = Var $ Sf Refs.smap Nothing
@@ -186,4 +196,111 @@ rewordExpression = runWriterT . para go
       VarF v -> pure $ Var v
       _ -> throwError $ "Expected var or apply"
 
+
+-- The yauhau node label type
+data YNL
+  = SfL QualifiedBinding (Maybe FnId)
+  | EnvL HostExpr
+  | LambdaL Assignment Expression
+
+-- A yauhau edge label
+type YEL = Maybe Binding
+
+type DepGr = Gr QualifiedBinding ()
+
+
+buildDependencyGraph :: DynGraph gr => Expression -> gr QualifiedBinding ()
+buildDependencyGraph = flip execState empty . flip runReaderT mempty . cata go
+  where
+    askSource b = asks $ join . HM.lookup b . fst
+    askDeps = asks snd
+    addDeps = local . second . maybe id (:)
+    registerSources bnds src = local $ first $ \hm -> foldr' (\k -> HM.insertWith (error "duplicate source") k src) hm bnds
+
+    go (VarF v) =
+      case v of
+        Local b -> askSource b
+        Sf _ Nothing -> error "No id"
+        Sf sf (Just (FnId sfid)) -> do
+          deps <- askDeps
+          modify $ insNode (sfid, sf)
+          modify $ insEdges $ map (sfid, , ()) deps
+          pure $ Just sfid
+        _ -> pure Nothing
+    go (ApplyF fn val) = do
+      valDeps <- val
+      addDeps valDeps fn
+    go (LetF assign val body) = do
+      source <- val
+      registerSources (flattenAssign assign) source body
+    go (LambdaF _ _) = pure Nothing
+
+
+focusGraph :: (Monoid b, DynGraph gr) => (LNode a -> Bool) -> gr a b -> gr a b
+focusGraph p gr = foldl' go gr $ map fst $ filter p $ labNodes gr
+  where
+    go gr' n = insEdges [ (source, target, a `mappend` b)
+                        | (source, a) <- lpre' ctx
+                        , (target, b) <- lsuc' ctx
+                        ]
+                        gr'
+      where
+        ctx = context gr' n
+
+fetchDependencyGraph :: (DynGraph gr, Monoid b) => gr QualifiedBinding b -> gr QualifiedBinding b
+fetchDependencyGraph = focusGraph (\(_, sf) -> sf == fetchName)
+
+
+contexts :: Graph gr => gr a b -> [Context a b]
+contexts = ufold (:) []
+
+
+calculateFetchRounds :: Graph gr => gr a b -> [IntSet.IntSet]
+calculateFetchRounds gr = unfold go initials
+  where
+    initials = IntSet.fromList $ map node' $ filter (null . suc') $ contexts gr
+    go vertices | IntSet.null new = Nil
+                | otherwise = Cons new new
+      where
+        new = IntSet.fromList $ IntSet.toList vertices >>= suc gr
+
+-- TODO improve:
+  -- there's a case where this transformation fails
+  -- This inserts the accumulator after the *last* expression that produces a value
+  -- a participating fetch depends on. However the graph we calculate before
+  -- does not respect the sequentiality of expressions.
+  -- it may therefore be possible that the result of a fetch is used before the last
+  -- dependency for the accumulator is produced.
+  -- This is because we suse the original expression, but we must in some cases reorder it.
+  -- I opt to ignore this problem for now.
+transform :: MonadGenBnd m => Expression -> m Expression
+transform e = foldl' rewriteRound e rounds
+  where
+    gr = buildDependencyGraph e
+    fetchGr = fetchDependencyGraph gr
+    rounds = calculateFetchRounds fetchGr
+
+    insertAcumExpr round rest = do
+      
+      Let rest
+      where 
+
+    rewriteRound e' round = flip runReader mempty $ flip runStateT False $ para go e
+      where
+        generic = fmap embed . traverse snd
+        deps = IntSet.fromList $ IntSet.toList round >>= pre gr
+        go e''@(LetF assign (Apply (Sf sf (Just i)) arg, val) (_, body)) = do
+          isDep <- asks $ IntSet.member i
+
+          if isDep
+            then local (IntSet.delete i) $ do
+              shouldInsert <- asks IntSet.null
+              if shouldInsert
+                then do
+                  v <- val
+                  b <- body
+                  insertAcumExpr round =<< Let assign v b
+                else Let assign <$> val <*> body
+            else generic e''
+        go e'' = generic e''
 
